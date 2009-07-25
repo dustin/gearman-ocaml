8aa6abd1d65733e9cf21f3a59879ed68adda1227
Dustin Sallings <dustin@spy.net>
2009-07-25 00:31:49 -0700

    More useful worker.

--- bin/worker.ml
+++ bin/worker.ml
@@ -2,14 +2,23 @@
  * Copyright 2009  Dustin Sallings <dustin@spy.net>
  *)
 
-let test_func s =
-  String.capitalize s
-
 let main () =
   let gearman = Gearman.connect Sys.argv.(1) 4730 in
 
-  Gearman.register gearman "test" test_func;
-  Gearman.do_work gearman;
+  (* Register string functions *)
+  Gearman.register gearman "uppercase" String.uppercase;
+  Gearman.register gearman "lowercase" String.lowercase;
+  Gearman.register gearman "capitalize" String.capitalize;
+  Gearman.register gearman "uncapitalize" String.uncapitalize;
+  Gearman.register gearman "escape" String.escaped;
+
+  (* Work for a bit *)
+  let rec loop n =
+    if (n > 0) then (
+      Gearman.do_work gearman;
+      Printf.printf "Did one job %d more to go\n%!" (n - 1);
+      loop (n - 1)) in
+  loop 10;
 
   Gearman.shutdown gearman
 ;;
